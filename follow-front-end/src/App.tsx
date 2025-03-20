c6f4b4ba5f611008cc24292a79dafdc459c8947c
Pablo <pablolucio_@hotmail.com>
2025-03-20 16:21:58 -0300

    chore: configures tailwindcss

--- follow-front-end/src/App.tsx
+++ follow-front-end/src/App.tsx
@@ -1,8 +1,8 @@
-import './index.css'
+import "./styles/index.css";
 
 function App() {
   return (
-    <main>
+    <main className="w-full h-full bg-primary">
       <h1>Follow Challenge</h1>
     </main>
   );
