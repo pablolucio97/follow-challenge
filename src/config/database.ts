11c13297f7507d5850248bcd40c07d1c7ab57524
Pablo <pablolucio_@hotmail.com>
2025-03-22 14:41:57 -0300

    chore: confgiures back-end

---
+++ src/config/database.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize";
+
+export const sequelizeDb = new Sequelize(
+  process.env.DB_NAME!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
+  {
+    host: "localhost",
+    port: 5432,
+    dialect: "postgres",
+    logging: false,
+  }
+);
